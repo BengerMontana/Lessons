410e85547fcc0e55793454d375359c795323ab56
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-01-26 01:29:06 +0200

    add create element tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+const list = React.createElement(
+    'ul',
+    null,
+    React.createElement('li', null, 'list item1'),
+    React.createElement('li', null, 'list item2'),
+    React.createElement('li', null, 'list item3')
+)
+
+const content = (
+    <div>
+        {h1}
+        {list}
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        {content}
+    </React.StrictMode>
+)
