c6bb6c8e7c2235f451a5eac40fcc3faa40b01a9a
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-01-26 03:14:53 +0200

    learn react.fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// function App() {
+//     return <h1>Hello app.js</h1>
+// }
+
+function Title() {
+    return <h1>Hello App.JS</h1>
+}
+function Content() {
+    return (
+        <div>
+            <p>Lorem</p>
+            <p>Lorem</p>
+        </div>
+    )
+}
+
+function TitleContent() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <TitleContent />
+    </React.StrictMode>
+)
+
 // const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
 
-const h1 = <h1 id="title">Hello React.js</h1>
-const a = 10
-const b = 'Product'
-const list = (
-    <ul id="list">
-        <li>Item {a}</li>
-        <li>{b} 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-    </ul>
-)
+// const h1 = <h1 id="title">Hello React.js</h1>
+// const a = 10
+// const b = 'Product'
+// const list = (
+//     <ul id="list">
+//         <li>Item {a}</li>
+//         <li>{b} 2</li>
+//         <li>Item 3</li>
+//         <li>Item 4</li>
+//     </ul>
+// )
 // const list = React.createElement(
 //     'ul',
 //     null,
@@ -22,11 +54,9 @@ const list = (
 //     React.createElement('li', null, 'list item3')
 // )
 
-const content = (
-    <div>
-        {h1}
-        {list}
-    </div>
-)
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+// const content = (
+//     <div>
+//         {h1}
+//         {list}
+//     </div>
+// )
