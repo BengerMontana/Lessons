f2730a5f05e09b24a97d4f95dacc1e037f93cb63
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-01-26 02:24:12 +0200

    rules fos JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
-const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list item1'),
-    React.createElement('li', null, 'list item2'),
-    React.createElement('li', null, 'list item3')
+// const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+
+const h1 = <h1 id="title">Hello React.js</h1>
+const a = 10
+const b = 'Product'
+const list = (
+    <ul id="list">
+        <li>Item {a}</li>
+        <li>{b} 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+    </ul>
 )
+// const list = React.createElement(
+//     'ul',
+//     null,
+//     React.createElement('li', null, 'list item1'),
+//     React.createElement('li', null, 'list item2'),
+//     React.createElement('li', null, 'list item3')
+// )
 
 const content = (
     <div>
@@ -17,8 +29,4 @@ const content = (
     </div>
 )
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        {content}
-    </React.StrictMode>
-)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
