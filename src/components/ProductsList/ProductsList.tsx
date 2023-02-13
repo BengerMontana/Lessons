610d8ee39cc01967d7d2d72128f90eacf17f9f7b
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-02-13 16:14:15 +0200

    add props to card

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -12,19 +12,43 @@ const ProductsList = (props: Props) => {
             <Grid container spacing={3} mt={5}>
                 <Grid item xs={12} sm={6} md={4}>
                     {''}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 11 pro"
+                        description="This is Iphone 11 pro"
+                        type="phone"
+                        capacity="256"
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {''}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 12 pro"
+                        description="This is Iphone 12 pro"
+                        type="phone"
+                        capacity="256"
+                        price={900}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {''}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 13 pro"
+                        description="This is Iphone 13 pro"
+                        type="phone"
+                        capacity="1024"
+                        price={1150}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {''}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone X"
+                        description="This is Iphone 10"
+                        type="phone"
+                        capacity="512"
+                        price={1500}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -1,18 +1,26 @@
 import { Card, CardContent, CardActions, Button } from '@mui/material'
 import '../ProductsList/ProductsListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 const ProductsListItem = (props: Props) => {
     return (
         <Card variant="outlined">
             <CardContent>
-                <div className="product-title">Iphone 14 pro</div>
-                <div className="product-desc">This Iphone 14 pro</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity: 256 Gb</div>
-                <div className="product-price">1200 $</div>
+                <div className="product-title">{props.title}</div>
+                <div className="product-desc">{props.description}</div>
+                <div className="product-features">Type: {props.type}</div>
+                <div className="product-features">
+                    Capacity: {props.capacity} Gb
+                </div>
+                <div className="product-price">{props.price} $</div>
             </CardContent>
-            <CardActions className='product-btn-wrap'>
+            <CardActions className="product-btn-wrap">
                 <Button variant="outlined">Add to cart</Button>
             </CardActions>
         </Card>
