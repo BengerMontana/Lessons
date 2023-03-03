7b4afc47fdcdbabc2971cf736f0a6a46e648c255
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-03-03 08:23:15 +0200

    add MenuItem

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,27 +1,17 @@
-import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
-import { Link } from 'react-router-dom'
 import '../../components/Menu/Menu.scss'
+import MenuItem from '../Menu/MenuItem'
+
 
 type Props = {}
 const Menu = (props: Props) => {
     return (
         <Stack direction="row" spacing={2} className="menu">
-            <Button color="inherit">
-                <Link to="/">Home</Link>
-            </Button>
-            <Button color="inherit">
-                <Link to="/about">About</Link>
-            </Button>
-            <Button color="inherit">
-                <Link to="/payment">Payment</Link>
-            </Button>
-            <Button color="inherit">
-                <Link to="/shipping">Shipping</Link>
-            </Button>
-            <Button color="inherit">
-                <Link to="/cart">Cart</Link>
-            </Button>
+            <MenuItem to="/">Home</MenuItem>
+            <MenuItem to="/about">About</MenuItem>
+            <MenuItem to="/payment">Payment</MenuItem>
+            <MenuItem to="/shipping">Shipping</MenuItem>
+            <MenuItem to="/cart">Cart</MenuItem>
         </Stack>
     )
 }
